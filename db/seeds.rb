7a045ea6b4b9007b841e1076c79316248b5dc4e7
Alexis Pipieri <alexis.pipieri@gmail.com>
2018-06-07 18:05:18 +0200

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,37 @@ puts 'Destroying PlaideurInterventions...'
 PlaideurIntervention.destroy_all
 puts 'Destroying users...'
 User.destroy_all
+
+file = Rails.root.join('db', 'seeds', 'seed.yml')
+sample = YAML.load(open(file).read)
+
+puts 'Creation écoles...'
+ecoles = {}
+sample["ecoles"].each do |ecole|
+  ecoles[ecole["nom"]] = Ecole.create! ecole.slice("nom", "adresse", "responsable", "email")
+end
+
+puts 'Creation thèmes...'
+themes = {}
+sample["themes"].each do |theme|
+  themes[theme["nom"]] = Theme.create! theme.slice("nom")
+end
+
+puts 'Creation users...'
+users = {}
+sample["users"].each do |user|
+  users[user["nom"]] = User.create! user.slice("prenom", "nom", "tel", "email", "password", "niveau", "admin")
+end
+
+
+puts 'Creating interventions...'
+interventions = {}
+sample["interventions"].each do |intervention|
+  theme = themes[intervention["theme"]]
+  ecole = ecoles[intervention["ecole"]]
+  interventions[intervention] = Intervention.create! intervention.slice("date_contact",
+    "date_intervention", "statut", "theme", "ecole").merge(theme: theme).merge(ecole: ecole)
+end
+
+puts 'Finished!'
+
