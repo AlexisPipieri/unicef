246aab9099ea948b9ae9dbbabf453dbf8e47e86f
Alexis Pipieri <alexis.pipieri@gmail.com>
2018-06-07 17:42:09 +0200

    adding routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'yaml'
+
+puts 'Destroying ecoles...'
+Ecole.destroy_all
+puts 'Destroying professeurs...'
+Professeur.destroy_all
+puts 'Destroying interventions...'
+Intervention.destroy_all
+puts 'Destroying themes...'
+Theme.destroy_all
+puts 'Destroying PlaideurInterventions...'
+PlaideurIntervention.destroy_all
+puts 'Destroying users...'
+User.destroy_all
