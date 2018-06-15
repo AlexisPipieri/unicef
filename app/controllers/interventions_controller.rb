a58817c19850fb7bfdc6ffb023d9a2008798a0d5
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-06-15 17:31:51 -0400

    adding search by plaideur name

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -2,10 +2,23 @@ class InterventionsController < ApplicationController
   skip_before_action :authenticate_user! # À enlever pour la mise en prod
   def index
     @interventions = Intervention.all
+    plaideurinterventions = PlaideurIntervention.all
     # raise
     if params[:query] && params[:query] != ''
-      @interventions = @interventions.search_interventions(params[:query])
+      if @interventions.search_interventions(params[:query]).empty?
+        matching_interventions = []
+        plaideurinterventions = plaideurinterventions.search_plaideur(params[:query])
+        plaideurinterventions.each do |plaideurintervention|
+          intervention = Intervention.find(plaideurintervention.intervention_id)
+          matching_interventions << intervention
+        end
+        @interventions = matching_interventions
+      else
+        @interventions = @interventions.search_interventions(params[:query])
+      end
     end
+
+    # mettre un scope de pgsearch dans plaideurintervention. Si le premier scope ne renvoie rien, alors chercher avec le deuxième scope
   end
 
 end
