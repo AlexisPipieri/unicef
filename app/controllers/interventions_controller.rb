5f5c069c7eb5420d72383d2add7476411ec1e2a4
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-06-28 15:40:03 +0200

    adding sorting by date + split between open and closed interventions, cleaning code

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,30 +1,31 @@
 class InterventionsController < ApplicationController
 
   def index
-    @interventions = Intervention.all.sort_by {|intervention| intervention.date_intervention.to_date}
+    @interventions = Intervention.all
+    @open_interventions = Intervention.where.not(statut: 'Terminée').sort_by {|intervention| intervention.date_intervention.to_date}
     @plaideurintervention = PlaideurIntervention.new
-    @pending_interventions = Intervention.where(statut: "A traiter")
-    @active_interventions = Intervention.where(statut: 'En cours')
-    @finished_interventions  = Intervention.where(statut: 'Terminé')
-    @unassigned_interventions = Intervention.where(statut: 'Non-assigné')
+    # @pending_interventions = Intervention.where(statut: "A traiter")
+    # @active_interventions = Intervention.where(statut: 'En cours')
+    @closed_interventions  = Intervention.where(statut: 'Terminée')
+    # @unassigned_interventions = Intervention.where(statut: 'Non-assigné')
 
 
     # search
     if params[:query] && params[:query] != ''
+      matching_interventions = []
       if @interventions.search_interventions(params[:query]).empty?
         plaideurinterventions = PlaideurIntervention.all
-        matching_interventions = []
         plaideurinterventions = plaideurinterventions.search_plaideur(params[:query])
         plaideurinterventions.each do |plaideurintervention|
           intervention = Intervention.find(plaideurintervention.intervention_id)
           matching_interventions << intervention
         end
-        @interventions = matching_interventions.sort_by {|intervention| intervention.date_intervention.to_date}
       else
-        @interventions = @interventions.search_interventions(params[:query]).sort_by {|intervention| intervention.date_intervention.to_date}
+        matching_interventions = @interventions.search_interventions(params[:query])
       end
+      @open_interventions = matching_interventions.select{|intervention| intervention.statut != 'Terminée'}.sort_by {|intervention| intervention.date_intervention.to_date}
+      @closed_interventions = matching_interventions.select{|intervention| intervention.statut == 'Terminée'}.sort_by {|intervention| intervention.date_intervention.to_date}
     end
-
     # assign
     @users_list = User.all
   end
